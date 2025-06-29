b2c794d9903dca58d9eea3e20d0e239c167753eb
NeoDimi <dimitri.lanari@gmail.com>
2025-06-29 20:05:54 +0200

    5. added movement functions to player.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print(pygame.init())
     clock = pygame.time.Clock() #create a clock object
     dt = 0 #delta time used to track frame time
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         #clock.tick(60) pauses the loop of 1/60 of a second. Effectively capping FPS to 60
         #divide by 1000 to go from ms to s and store in dt for tracking
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 
 # Base class for game objects
@@ -7,7 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
+    
 # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -16,10 +16,30 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def rotate_triangle(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
-        # sub-classes must override
-        pass
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate_triangle(-dt)
+        if keys[pygame.K_d]:
+            self.rotate_triangle(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        #would be used as a "reset position" button
+        #if keys[pygame.K_c]:
+        #   self.rotation = 0
+        #   self.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
